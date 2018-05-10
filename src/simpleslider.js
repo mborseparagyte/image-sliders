bef6d582b505d1f0924997b5d41f370c04c80f87
Manoj Borse <mborse@paragyte.com>
2018-05-10 18:04:44 +0530

    added imageloader

--- src/simpleslider.js
+++ src/simpleslider.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import ImageLoader from 'react-imageloader';
 class SimpleSlider extends Component {
+  preloader() {
+    return <img src="spinner.gif" />;
+  }
   render() {
     const settings = {
       infinite: true,
-      autoplay: true,
+      //autoplay: true,
       speed: 100,
       slidesToShow: 1,
       slidesToScroll: 1
@@ -19,12 +23,23 @@ class SimpleSlider extends Component {
               {images.map(image => {
                 return (
                   <div className="simple-Slick-item">
-                    <img
+                    <ImageLoader
+                      src={`${siteURL}/${image.image}`}
+                      wrapper={React.createFactory('div')}
+                      style={{
+                        height: 400,
+                        width: "100%"
+                      }}
+                      preloader={this.preloader}>
+
+                      {"Image load failed!"}
+                    </ImageLoader>
+                    {/*<img
                       height="400px"
                       width="100%"
                       alt=""
                       src={`${siteURL}/${image.image}`}
-                    />
+                    />*/}
                   </div>
                 );
               })}
