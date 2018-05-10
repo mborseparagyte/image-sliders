b100dab9926f148be0a84b89c614b3e3450ebd15
Manoj Borse <mborse@paragyte.com>
2018-05-10 13:57:37 +0530

    imageryTitle added

---
+++ src/imageryTitle.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+class ImageryTitle extends Component {
+  render() {
+    const settings = {
+      infinite: true,
+      //autoplay: true,
+      speed: 200,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    let { images, siteURL } = this.props;
+    return (
+      <div>
+        <h4></h4>
+        <div className="">
+          <div>
+          {"Imagery Title"}
+          </div>
+          <div className="row" style={{ margin: "0px 20px" }}>
+            {images.map(image => {
+              return (
+                <div className="col-xs imageryImg">
+                  <img height="250px" width="100%" alt="" src={`${siteURL}/${image.image}`} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default ImageryTitle;
--- src/index.js
+++ src/index.js
@@ -9,40 +9,122 @@ import SimpleSlider from "./simpleslider.js";
 import Featured from "./featured.js";
 import Trending from "./trending.js";
 import Imagery from "./imagery.js";
+import ImageryTitle from "./imageryTitle.js";
 const styles = {
   fontFamily: "sans-serif",
   textAlign: "center"
 };
 const images = [
-  { image: "images/slide_1.jpg", title: "Sparx, Stylar & more", description: "Upto 60+Extra 10% Off", category: "Slippers & Sandals" },
-  { image: "images/slide_2.jpg", title: "Puma, Adidas & more", description: "Under ₹1999+Extra 10%", category: "Men's Footwear" },
-  { image: "images/slide_3.jpg", title: "Aero, Kraasa & more", description: "Under ₹1499+Extra10% ", category: "Men's Shoes" },
-  { image: "images/slide_4.jpg", title: "Oomph!, Ishin, Reya...", description: "Min 60% Off", category: "Sarees, Dress Materials..." },
-  { image: "images/slide_5.jpg", title: "3D Printed Bedsheets", description: "₹169-₹399", category: "Colorful Collection" },
-  { image: "images/slide_6.jpg", title: "Bath Furnishing Range", description: "From ₹149", category: "Towels, Bath Robes & more" },
-  { image: "images/slide_7.jpg", title: "Wall Stickers ", description: "From ₹109", category: "Flipkart SmartBuy" }
+    {
+      image: "images/slide_1.jpg",
+      title: "Sparx, Stylar & more",
+      description: "Upto 60+Extra 10% Off",
+      category: "Slippers & Sandals"
+    },
+    {
+      image: "images/slide_2.jpg",
+      title: "Puma, Adidas & more",
+      description: "Under ₹1999+Extra 10%",
+      category: "Men's Footwear"
+    },
+    {
+      image: "images/slide_3.jpg",
+      title: "Aero, Kraasa & more",
+      description: "Under ₹1499+Extra10% ",
+      category: "Men's Shoes"
+    },
+    {
+      image: "images/slide_4.jpg",
+      title: "Oomph!, Ishin, Reya...",
+      description: "Min 60% Off",
+      category: "Sarees, Dress Materials..."
+    },
+    {
+      image: "images/slide_5.jpg",
+      title: "3D Printed Bedsheets",
+      description: "₹169-₹399",
+      category: "Colorful Collection"
+    },
+    {
+      image: "images/slide_6.jpg",
+      title: "Bath Furnishing Range",
+      description: "From ₹149",
+      category: "Towels, Bath Robes & more"
+    },
+    {
+      image: "images/slide_7.jpg",
+      title: "Wall Stickers ",
+      description: "From ₹109",
+      category: "Flipkart SmartBuy"
+    }
   ],
   imageryImages = [
-    { image: "images/slide_1.jpg", title: "Sparx, Stylar & more", description: "Upto 60+Extra 10% Off", category: "Slippers & Sandals" },
-    { image: "images/slide_2.jpg", title: "Puma, Adidas & more", description: "Under ₹1999+Extra 10%", category: "Men's Footwear" },
-    { image: "images/slide_3.jpg", title: "Aero, Kraasa & more", description: "Under ₹1499+Extra10% ", category: "Men's Shoes" },
+    {
+      image: "images/slide_1.jpg",
+      title: "Sparx, Stylar & more",
+      description: "Upto 60+Extra 10% Off",
+      category: "Slippers & Sandals"
+    },
+    {
+      image: "images/slide_2.jpg",
+      title: "Puma, Adidas & more",
+      description: "Under ₹1999+Extra 10%",
+      category: "Men's Footwear"
+    },
+    {
+      image: "images/slide_3.jpg",
+      title: "Aero, Kraasa & more",
+      description: "Under ₹1499+Extra10% ",
+      category: "Men's Shoes"
+    }
+  ],
+  imageryTitleImages = [
+    {
+      image: "images/slide_1.jpg",
+      title: "Sparx, Stylar & more",
+      description: "Upto 60+Extra 10% Off",
+      category: "Slippers & Sandals"
+    },
+    {
+      image: "images/slide_2.jpg",
+      title: "Puma, Adidas & more",
+      description: "Under ₹1999+Extra 10%",
+      category: "Men's Footwear"
+    },
+    {
+      image: "images/slide_3.jpg",
+      title: "Aero, Kraasa & more",
+      description: "Under ₹1499+Extra10% ",
+      category: "Men's Shoes"
+    },
+    {
+      image: "images/slide_4.jpg",
+      title: "Oomph!, Ishin, Reya...",
+      description: "Min 60% Off",
+      category: "Sarees, Dress Materials..."
+    }
   ],
   siteURL = `https://react-slideshow.herokuapp.com`;
 //expireTime in ISO string
 const App = () => (
   <div style={styles}>
     <SimpleSlider images={images} siteURL={siteURL} />
-    <Imagery images={imageryImages} siteURL={siteURL}/>
+    <Imagery images={imageryImages} siteURL={siteURL} />
+    <ImageryTitle
+      images={imageryTitleImages}
+      siteURL={siteURL}
+      sectionTitle={"Featured Brands"}
+    />
     <Featured
       images={images}
-      sectionTitle ={"Deals of the Day"}
+      sectionTitle={"Deals of the Day"}
       siteURL={siteURL}
       buttonLabel={"View All"}
       expireTime={"2018-05-10T12:28:03.000Z"}
       noOfProductsToShow={6}
     />
     <Trending
-      sectionTitle = {"Trending Offers"}
+      sectionTitle={"Trending Offers"}
       images={images}
       siteURL={siteURL}
       buttonLabel={"View All"}
